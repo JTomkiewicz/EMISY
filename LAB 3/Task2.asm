e6ca0883e887e2864305f69b031168f65178d3a2
Jakub Tomkiewicz <jakub.tomkiewicz@elektrometal-energetyka.pl>
2021-05-24 18:39:10 +0200

    nearly working func

--- LAB 3/Task2.asm
+++ LAB 3/Task2.asm
@@ -2,8 +2,25 @@
 
 ;labels definitions
 LED_BUS EQU P1 ;pins from P1.0 to P1.7
+INT1 EQU P3.3 ;INT1 external interrupt
+KBRD_BUS EQU P0 ;keyboard bus pins from P0.0 to P0.7
 
-;send 1s to display to make it clear -> nothing on display
-mov LED_BUS, #00000000B
+mov KBRD_BUS, #11110000B
 
-jmp $ ;inifinite loop at the end, jump to yourselfe
+;jump to configuration
+jmp configurationFirst
+
+org 013H ;INT1 external interrupt
+
+xrl LED_BUS, #11111111B
+jnb INT1, $
+
+reti ;return interrupt subroutine
+
+configurationFirst:
+	setb EA ;global interrupt ENABLE
+	setb EX1 ;INT1 interrupt ENABLE
+	setb IT1 ;falling edge on input signal
+
+	jmp $ ;inifinite loop at the end, jump to yourselfe
+
